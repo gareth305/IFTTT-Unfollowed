38502b5b999b41a7644be618f5f5d288b346ca9c
Gaz <gareth.p305@gmail.com>
2019-05-24 14:47:56 +0100

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@
  * access_token_secret
  */
 module.exports = {
-  IFTTT_KEY: 'cCB65FoeW0x6cQCWgb_b-3',
-  consumer_key: 'xRibewtuwVLfOlmDAaksiwwxh',
-  consumer_secret: 'ScXOd1Dd5EpvWMtWdkQsz2jSVueYVmhUvL5mrQwAmGqm1TvX4K',
-  access_token: '211173647-CT3gUOUes7hqHntxN13rHVnmEkHTCPO9orrECgG0',
-  access_token_secret: 'fTohVQFMv6pAek1SnJUc0WY24teUBhsyzvxlbFJ0Uaa8m'
+  IFTTT_KEY: 'xxxxxxxxxxxxx',
+  consumer_key: 'xxxxxxxxxxxxx',
+  consumer_secret: 'xxxxxxxxxxxxx',
+  access_token: 'xxxxxxxxxxxxx',
+  access_token_secret: 'xxxxxxxxxxxxx'
 }
